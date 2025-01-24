112604aba74e9011545f6f31a6c063882be2a2a6
mrtolkien <gary.mialaret@gmail.com>
2025-01-24 13:07:19 +0900

    feat: pre-select biggest image

--- src/immich_python_scripts/duplicates/widget.py
+++ src/immich_python_scripts/duplicates/widget.py
@@ -12,22 +12,31 @@ class DuplicatesHandler:
     def load_next(self, table: DataTable):
         if self.current_index >= len(self.duplicates):
             table.clear()
-            return False
+            return None
 
         duplicate_assets = self.duplicates[self.current_index]
         table.clear()
         self.current_albums = set()
 
-        for asset in duplicate_assets.assets:
+        largest_asset_id = 0
+        largest_size = -1
+
+        for idx, asset in enumerate(duplicate_assets.assets):
             asset_album_ids = [a.id for a in api.queries.get_albums(asset.id)]
             self.current_albums.update(asset_album_ids)
 
             if asset.exifInfo is None:
                 file_size = "N/A"
                 resolution = "N/A"
+                size_in_bytes = -1
             else:
-                file_size = format_file_size(asset.exifInfo.fileSizeInByte)
+                size_in_bytes = asset.exifInfo.fileSizeInByte
+                file_size = format_file_size(size_in_bytes)
                 resolution = f"{int(asset.exifInfo.exifImageWidth or 0)}x{int(asset.exifInfo.exifImageHeight or 0 )}"
+
+            if size_in_bytes > largest_size:  # type: ignore
+                largest_size = size_in_bytes
+                largest_asset_id = idx
 
             if not table.columns:
                 table.add_columns("Filename", "Size", "Resolution", "Albums")
@@ -40,7 +49,7 @@ class DuplicatesHandler:
                 key=asset.id,
             )
 
-        return True
+        table.move_cursor(row=largest_asset_id)
 
     def handle_selection(self, asset_id):
         # albums = self.current_albums
