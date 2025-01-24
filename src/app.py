cb80d46b83c65202a02b5c27f4b34db4c137f394
mrtolkien <gary.mialaret@gmail.com>
2025-01-24 23:33:38 +0900

    chore: presentation on start

--- src/app.py
+++ src/app.py
@@ -1,14 +1,46 @@
+import os
+
 import questionary
+from rich.console import Console
+from rich.markdown import Markdown
 
 from immich_python_scripts import duplicates
 
 
+def show_presentation():
+    Console().print(
+        Markdown("""
+# Immich Python Scripts
+
+Simply manage your Immich duplicates. 
+
+## Features
+
+### Duplicate Management
+- **Step by Step Review**: Review and merge duplicates one by one
+  - Compare metadata and thumbnails
+  - Pick which file to keep
+  - Merge album memberships
+  - Move others to trash
+- **Automatic Merge**: Automatically merge duplicates
+  - Keeps the highest quality version
+  - Merges album memberships
+  - Moves others to trash
+
+---
+""")
+    )
+
+
 def main():
+    os.system("clear")
+    show_presentation()
+
     response = questionary.select(
         "What do you want to do?",
         choices=[
-            "Review duplicates one by one with album merging",
-            "Automatically merge duplicates with album merging",
+            "Review duplicates one by one",
+            "Automatically merge duplicates",
             "Exit",
         ],
     ).ask()
