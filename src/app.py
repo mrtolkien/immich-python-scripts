9a8431bd09b9501fdfcbcd8e64f505841abd9f31
mrtolkien <gary.mialaret@gmail.com>
2025-01-24 12:47:00 +0900

    feat: good start

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from textual.app import App
 from textual.containers import Container
-from textual.widgets import Footer, OptionList
+from textual.widgets import DataTable, Footer, Header, OptionList
 from textual.widgets.option_list import Option, Separator
 
 import immich_python_scripts.duplicates
@@ -8,8 +8,11 @@ import immich_python_scripts.duplicates
 
 class ImmichApp(App):
     BINDINGS = [("q", "quit", "Quit")]
+    CSS_PATH = "app.css"
 
     def compose(self):
+        yield Header(show_clock=True, name="Immich")
+
         yield Container(
             OptionList(
                 Option(
@@ -20,18 +23,32 @@ class ImmichApp(App):
                 Option("Exit", id="exit"),
                 id="menu",
             ),
+            DataTable(cursor_type="row", show_cursor=True),
+            id="content",
         )
 
         yield Footer()
 
+    def on_mount(self):
+        self.query_one("#menu").focus()
+        self.title = "Immich Python Scripts"
+
     def on_option_list_option_selected(self, event):
-        return self.exit(event.option.id)
+        self.query_one("#menu").remove()
+
+        if event.option.id == "duplicates":
+            self.title = "Duplicates removal"
+            self.sub_title = "The selected asset gets added to albums"
+            immich_python_scripts.duplicates.run(self.query_one(DataTable))
+
+        elif event.option.id == "exit":
+            self.exit()
+
+    def on_data_table_row_selected(self, event):
+        print(event.row_key)
+        self.exit()
 
 
 def main():
     app = ImmichApp()
-
-    result = app.run()
-
-    if result == "duplicates":
-        immich_python_scripts.duplicates.run()
+    app.run()
--- src/immich_python_scripts/duplicates/run.py
+++ src/immich_python_scripts/duplicates/run.py
@@ -1,15 +1,43 @@
+from textual.widget import Widget
+from textual.widgets import DataTable
+
 from .. import api
 
 
-def run_duplicates():
+def run_duplicates(table: DataTable):
     duplicates = api.queries.get_duplicates()
 
+    table.add_columns("Filename", "Size", "Resolution", "Albums")
+
     for duplicate_assets in duplicates:
+        table.clear()
         albums = set()
 
         for asset in duplicate_assets.assets:
-            albums.update(api.queries.get_albums(asset.id))
+            asset_album_ids = [a.id for a in api.queries.get_albums(asset.id)]
+            albums.update(asset_album_ids)
 
-        print(albums)
+            if asset.exifInfo is None:
+                file_size = "N/A"
+                resolution = "N/A"
+            else:
+                file_size = format_file_size(asset.exifInfo.fileSizeInByte)
+                resolution = f"{int(asset.exifInfo.exifImageWidth or 0)}x{int(asset.exifInfo.exifImageHeight or 0 )}"
+
+            table.add_row(
+                asset.originalFileName,
+                file_size,
+                resolution,
+                len(asset_album_ids),
+                key=asset.id,
+            )
 
         break
+
+
+def format_file_size(size_in_bytes):
+    for unit in ["B", "KB", "MB", "GB", "TB"]:
+        if size_in_bytes < 1024.0:
+            return f"{size_in_bytes:.2f} {unit}"
+        size_in_bytes /= 1024.0
+    return f"{size_in_bytes:.2f} PB"
